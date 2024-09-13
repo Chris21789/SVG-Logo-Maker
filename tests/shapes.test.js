0a8c29fc471555e9f24c153cffb98fa789e7bc7e
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-13 03:40:41 -0400

    recreated the shapes.js file in a way that made more sense to me. added code for each shape object, ran jest and passed all three tests.

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,26 +1,25 @@
-const { describe } = require('yargs');
-const {Triangle, Circle, Square} = require('./shapes.js');
+const {Triangle, Circle, Square} = require('../lib/shapes.js');
 
 describe('Triangle', () =>{
     test('the color of the triangle should be the blue from the shape.setColor("blue") method', () => {
         const shape = new Triangle();
         shape.setColor("blue");
         expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
-    })
-})
+    });
+});
 
 describe('Circle', () =>{
     test('the color of the circle should be the orange from the shape.setColor("orange") method', () => {
         const shape = new Circle();
         shape.setColor("orange");
-        expect(shape.render()).toEqual('<circle cx="25" cy="75" r="20" />');
-    })
-})
+        expect(shape.render()).toEqual('<circle cx="25" cy="75" r="20" fill="orange" />');
+    });
+});
 
 describe('Square', () =>{
     test('the color of the square should be the green from the shape.setColor("green") method', () => {
         const shape = new Square();
         shape.setColor("green");
-        expect(shape.render()).toEqual('<rect x="10" y="10" width="30" height="30" />');
-    })
-})
+        expect(shape.render()).toEqual('<rect x="10" y="10" width="30" height="30" fill="green" />');
+    });
+});
