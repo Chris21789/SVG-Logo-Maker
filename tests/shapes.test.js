aedc6842027f1ffbbe126b4aab8977894ab2feb7
Chris <ccolon29@mail.valenciacollege.edu>
2024-09-17 10:11:02 -0400

    adjusted the values for the shapes so they would fit better in the frame. adjust the tests to reflect the change.

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -4,7 +4,7 @@ describe('Triangle', () =>{
     test('the color of the triangle should be the blue from the shape.setColor("blue") method', () => {
         const shape = new Triangle();
         shape.setColor("blue");
-        expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+        expect(shape.render()).toEqual('<polygon points="50,150 250,150 150,0" fill="blue" />');
     });
 });
 
@@ -12,7 +12,7 @@ describe('Circle', () =>{
     test('the color of the circle should be the orange from the shape.setColor("orange") method', () => {
         const shape = new Circle();
         shape.setColor("orange");
-        expect(shape.render()).toEqual('<circle cx="25" cy="75" r="20" fill="orange" />');
+        expect(shape.render()).toEqual('<circle cx="150" cy="100" r="70" fill="orange" />');
     });
 });
 
@@ -20,6 +20,6 @@ describe('Square', () =>{
     test('the color of the square should be the green from the shape.setColor("green") method', () => {
         const shape = new Square();
         shape.setColor("green");
-        expect(shape.render()).toEqual('<rect x="10" y="10" width="30" height="30" fill="green" />');
+        expect(shape.render()).toEqual('<rect x="100" y="50" width="100" height="100" fill="green" />');
     });
 });
